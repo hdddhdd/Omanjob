ed211447dfc74677b95f2160e1e9175ecdac873a
hdddhdd <nuly08@naver.com>
2022-11-02 21:46:27 +0900

    commit 수정

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from unicodedata import name
+#from django.conf.urls import url
+from django.urls import re_path as url
+
+from . import views
+
+urlpatterns = [
+
+    path('', views.main, name='main'),
+    path('test', views.test, name='test'),
+    path('play', views.play, name='play'),
+]
